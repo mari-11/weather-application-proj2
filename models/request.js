a69b12da6bd06151723f7824bc51861c7117f92a
Mari-11 <marigarden707@gmail.com>
2021-08-27 19:20:00 -0700

    request-file

--- models/request.js
+++ models/request.js
@@ -4,4 +4,33 @@ module.exports = function(sequelize, DataTypes) {
         type: DataTypes.STRING,
         allowNull: false
       },
-   
+    },
+    answer: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+    // ,
+    // createdAt: {
+    //   type: DataTypes.DATE, 
+    //   allowNull: false, 
+    //   defaultValue: sequelize.fn('NOW')
+    // }
+    // ,
+    // users_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: 'persons',
+    //   referencesKey: 'id'
+    // }
+  },{
+  	classMethods: {
+  		associate: (models) => {
+  			Request.belongsTo(models.User, {
+  				foreignKey: 'user_id'
+  				// ,
+  				// as: 'user_id'
+  			})
+  		}
+  	}
+  });
+  return Request;
+}
